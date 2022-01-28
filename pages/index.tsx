aaed71102b06c56fdc87436cb72e69a03ba28ff3
jjenna <jenna@solidware.io>
2022-01-28 14:34:36 +0900

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>JJuhey</title>
+      </Head>
+
+      <main className={styles.main}>
+        🚧🛠 UNDER CONSTRUCTION 🛠🚧
+      </main>
+
+      <footer className={styles.footer}>This is JJuhey's Blog</footer>
+    </div>
+  )
+}
