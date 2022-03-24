fabae93e159aea4363bc79acc504273fd22bb4dc
jjuhey <53938072+JJuhey@users.noreply.github.com>
2022-03-24 12:17:39 +0900

    feat: apply material-ui and Make layout (#4)
    * Apply material-ui and Make layout
    
    * Add 2nd post
    
    * Fix typo
    
    * Add key prop
    
    * Add contents for second post
    
    * fix: show correctly on mobile
    
    * chore: change style
    
    Co-authored-by: jjenna <jenna@solidware.io>

---
+++ components/IntroductionCard.tsx
@@ -0,0 +1,33 @@
+import Image from 'next/image'
+import { Paper, Avatar, Chip } from '@material-ui/core'
+
+import layoutStyle from '../styles/Layout.module.css'
+
+const IntroductionCard = () => {
+  return (
+    <Paper elevation={3} style={{ height: '400px', padding: '10px' }}>
+      <Image
+        priority
+        src="/images/profile.jpeg"
+        className={layoutStyle.borderCircle}
+        height={144}
+        width={144}
+        alt='jjuhey'
+      />
+      <div style={{ fontSize: '1.2rem' }}>JJUHEY CHOI</div>
+      <div style={{ fontSize: '0.8rem' }}>Web developer</div>
+      {/* <Avatar alt="jjuhey" src='../public/vercel.svg'>Ju</Avatar> */}
+      <div style={{ padding: '10px' }}>
+        <Chip style={{ marginRight: '5px' }} size='small' label="JavaScript" />
+        <Chip style={{ marginRight: '5px' }} size='small' label="TypeScript" />
+        <Chip style={{ marginRight: '5px' }} size='small' label="Nodejs" />
+        <Chip style={{ marginRight: '5px' }} size='small' label="React" />
+        <Chip style={{ marginRight: '5px' }} size='small' label="MongoDB" />
+        <Chip style={{ marginRight: '5px' }} size='small' label="CSS" />
+        <Chip size='small' label="HTML" />
+      </div>
+    </Paper>
+  )
+}
+
+export default IntroductionCard;
---
+++ posts/second-post.md
@@ -0,0 +1,11 @@
+---
+title: 'Study Record 2022'
+date: '2022-02-25'
+---
+
+**한 달에 한 번씩 기록하는 스터디 기록[2/12]**
+- 홈페이지에 레이아웃 작업 [**[#4]**](https://github.com/JJuhey/nextjs-jjuhey/pull/4)
+  - material ui 적용
+  - 레이아웃, 컴포넌트
+- 파일정리 스크립트 만들어보기 _**<https://github.com/JJuhey/organize-file>**_
+  - inquirer 이용
